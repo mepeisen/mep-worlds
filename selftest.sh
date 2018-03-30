8446239c280232f21f61896790552a1f9127eb29
mepeisen <mep_eisen@web.de>
2018-03-30 08:09:03 +0200

    fix

--- selftest.sh
+++ selftest.sh
@@ -9,7 +9,7 @@ mkdir ccemu
 HOME=$PWD/bin/ccemu	
 
 echo "using Display: $DISPLAY"
-flvrec.py -o record.flv -r 60 -P ../vnc-pass.txt localhost$DISPLAY &
+flvrec.py -o record.flv -r 200 -P ../vnc-pass.txt localhost$DISPLAY &
 THE_PID=$!
 
 # Xvfb :99 -screen 0 1024x768x24 &
@@ -22,7 +22,7 @@ sleep 1s
 xdotool key c d space p r o g r a m s Return
 sleep 0.1s
 
-xdotool key x w o s _ s e l f t e s t Return
+xdotool key x w o s underscore s e l f t e s t Return
 sleep 1.5s
 
 xdotool key s h u t d o w n Return
