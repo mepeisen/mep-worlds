960b6e87d14d56c8398b4577f64de894a5e5c818
mepeisen <mep_eisen@web.de>
2018-03-30 07:51:50 +0200

    fix

--- selftest.sh
+++ selftest.sh
@@ -8,7 +8,7 @@ mkdir ccemu
 
 HOME=$PWD/bin/ccemu	
 
-python flvrec.py -o record.flv -r 60 &
+flvrec.py -o record.flv -r 60 &
 THE_PID=$!
 
 # Xvfb :99 -screen 0 1024x768x24 &
@@ -16,75 +16,15 @@ THE_PID=$!
 # DISPLAY=:99
 sleep 0.5s
 love /srv/lua/cclite/1.7/cclite-latest-beta.love &
-#import -window root screenshot_001.png
 sleep 1s
-#import -window root screenshot_002.png
 
-xdotool key c
-xdotool key d
-xdotool key Space
-xdotool key p
-xdotool key r
-xdotool key o
-xdotool key g
-xdotool key r
-xdotool key a
-xdotool key m
-xdotool key s
-xdotool key RETURN
-#import -window root screenshot_010.png
+xdotool key c d space p r o g r a m s Return
 sleep 0.1s
-#import -window root screenshot_011.png
 
-xdotool key x
-xdotool key w
-xdotool key o
-xdotool key s
-xdotool key _
-xdotool key s
-xdotool key e
-xdotool key l
-xdotool key f
-xdotool key t
-xdotool key e
-xdotool key s
-xdotool key t
-xdotool key RETURN
-#import -window root screenshot_100.png
-sleep 0.1s
-#import -window root screenshot_101.png
-sleep 0.1s
-#import -window root screenshot_102.png
-sleep 0.1s
-#import -window root screenshot_103.png
-sleep 0.1s
-#import -window root screenshot_104.png
-sleep 0.1s
-#import -window root screenshot_105.png
-sleep 0.1s
-#import -window root screenshot_106.png
-sleep 0.3s
-#import -window root screenshot_107.png
-sleep 0.3s
-#import -window root screenshot_108.png
-sleep 0.3s
-#import -window root screenshot_109.png
-sleep 0.3s
-#import -window root screenshot_110.png
+xdotool key x w o s _ s e l f t e s t Return
+sleep 1.5s
 
-xdotool key s
-xdotool key h
-xdotool key u
-xdotool key t
-xdotool key d
-xdotool key o
-xdotool key w
-xdotool key n
-xdotool key RETURN
-#import -window root screenshot_200.png
-sleep 0.3s
-#import -window root screenshot_201.png
-sleep 0.3s
-#import -window root screenshot_202.png
+xdotool key s h u t d o w n Return
+sleep 0.5s
 
 kill -4 $THE_PID
