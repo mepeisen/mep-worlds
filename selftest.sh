45819d2808bbb4fa7db9e916e9ce19995f07e3a8
mepeisen <mep_eisen@web.de>
2018-03-30 08:17:55 +0200

    fix

--- selftest.sh
+++ selftest.sh
@@ -5,11 +5,11 @@ fi
 mkdir bin
 cd bin
 
-if [ -d "~/.local/share/love/ccemu/data/0" ]; then
-	rm -rf "~/.local/share/love/ccemu/data/0"
+if [ -d "/var/lib/jenkins/.local/share/love/ccemu/data/0" ]; then
+	rm -rf "/var/lib/jenkins/.local/share/love/ccemu/data/0"
 fi
-mkdir "~/.local/share/love/ccemu/data/0"
-cp -r "../src/assets/computercraft/lua/rom" "~/.local/share/love/ccemu/data/0"
+mkdir "/var/lib/jenkins/.local/share/love/ccemu/data/0"
+cp -r "../src/assets/computercraft/lua/rom" "/var/lib/jenkins/.local/share/love/ccemu/data/0"
 
 echo "using Display: $DISPLAY"
 flvrec.py -o record.flv -P ../vnc-pass.txt localhost$DISPLAY &
@@ -18,17 +18,17 @@ THE_PID=$!
 # Xvfb :99 -screen 0 1024x768x24 &
 # THE_PID=$!
 # DISPLAY=:99
-sleep 0.5s
-love /srv/lua/cclite/1.7/cclite-latest-beta.love &
 sleep 1s
+love /srv/lua/cclite/1.7/cclite-latest-beta.love &
+sleep 5s
 
 xdotool key c d space p r o g r a m s Return
-sleep 0.1s
+sleep 2s
 
 xdotool key x w o s underscore s e l f t e s t Return
-sleep 1.5s
+sleep 10s
 
 xdotool key s h u t d o w n Return
-sleep 0.5s
+sleep 2s
 
 kill -4 $THE_PID
